49adecf7c845b8ca1b8b4ae0f296e3498ee53d39
Calainkim <_>
2025-07-24 16:05:59 +0900

    retry git

--- Java-Study/src/variable/Ex22.java
+++ Java-Study/src/variable/Ex22.java
@@ -4,5 +4,6 @@ public class Ex22 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("commit test");
+        System.out.println("retry git");
     }
 }
