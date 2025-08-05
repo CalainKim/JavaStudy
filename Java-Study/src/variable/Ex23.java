726d17a0bbbec04bd593b3f617ec3dbbf2fbf824
Calainkim <_>
2025-08-05 14:12:25 +0900

    안녕하세요

---
+++ Java-Study/src/variable/Ex23.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Ex23 {
+    public static void main(String[] args) {
+        int []numbers = {1,2,3,4,5};
+
+        for(int number : numbers){
+            System.out.print(number+" ");
+        }
+    }
+}
