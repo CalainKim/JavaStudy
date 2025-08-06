eb1fa9adfb1947f4013cc5f4ab3ac9e764558328
CalainKim <doeun110011@gmail.com>
2025-08-06 20:50:32 +0900

    github test2

--- Java-Study/src/Java_basic/StudentClass.java
+++ Java-Study/src/Java_basic/StudentClass.java
@@ -4,11 +4,11 @@ public class StudentClass {
     public static void main(String[] args) {
         StudentInfor student1 = new StudentInfor();
         student1.name = "도은";
-        student1.age = 22; // ss
+        student1.age = 22;
         student1.grade = 100;
 
 
-        StudentInfor student2 = new StudentInfor(); // github test
+        StudentInfor student2 = new StudentInfor(); // github test ss
         student2.name = "은도";
         student2.age = 24;
         student2.grade = 90;
